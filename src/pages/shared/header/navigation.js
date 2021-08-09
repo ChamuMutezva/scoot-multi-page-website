b83533a32976daebc1199f27c3daef9180fe5f5b
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-08-09 07:19:48 +0200

    home page navigation and hero section complete

--- src/pages/shared/header/navigation.js
+++ src/pages/shared/header/navigation.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import BtnScoot from './BtnScoot'
 import Hamburger from '../../../assets/icons/hamburger.svg'
@@ -5,13 +6,20 @@ import Close from '../../../assets/icons/close.svg'
 import Logo from '../../../assets/logo.svg'
 import NavList from './navList'
 const Navigation = () => {
+    const [menu, setMenu] = useState(true)
+
+    const handleClick = () => {
+        setMenu(!menu)
+        console.log("button clicked")
+    }
+
     const trial = document.querySelector;
     console.log(typeof trial)
     return (
         <nav className="nav" role="navigation" aria-label="main navigation">
             {/*Opening and closing the menu */}
             <div className="menu__control">
-                <button className="show__menu" aria-label="open navigation" aria-expanded="false">
+                <button className="show__menu" aria-label="open navigation" aria-expanded="false" onClick={handleClick}>
                     <img className="menu__img" src={Hamburger} alt="" />
                 </button>
                 <button className="close__menu" aria-label="close the navigation" aria-expanded="true">
@@ -29,7 +37,7 @@ const Navigation = () => {
             {/* ---------------------------------*/}
 
             {/*Modal and navigation list items*/}
-            <div className="modal__container">
+            <div className={`modal__container ${menu ? "hide__modal" : ""} `}>
                 <NavList />
                 <BtnScoot />
             </div>
